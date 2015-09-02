e8f3a53036e374a0be1beb88ac861295120fb496
Steve Ipatov <ipatov@ispras.ru>
2015-09-02 20:08:03 +0300

    new webui is on its way

---
+++ webui/Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    '../lingvodoc/static/css/lingvodoc.css': 'src/sass/lingvodoc.scss'
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed',
+                    loadPath: 'bower_components/bootstrap-sass/assets/stylesheets/bootstrap'
+                },
+                files: {
+                    '../lingvodoc/static/css/lingvodoc.css': 'src/sass/lingvodoc.scss'
+                }
+
+            },
+
+        },
+        watch: {
+            sass: {
+                files: 'src/sass/*.scss',
+                tasks: ['sass:dev']
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, flatten: true, src: ['bower_components/bootstrap-sass/assets/fonts/bootstrap/*'], dest: '../lingvodoc/static/fonts/', filter: 'isFile'},
+                ],
+            },
+        },
+        uglify: {
+            options: {
+                compress: true
+            },
+            lingvodoc: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/angular/angular.js',
+                    'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+                    'bower_components/wavesurfer.js/dist/wavesurfer.min.js',
+                    'src/js/lingvodemo.js',
+                    'src/js/lingvodocedit.js',
+                    'src/js/lingvodocview.js',
+                    'src/js/lingvowave.js'
+                ],
+                dest: '../lingvodoc/static/js/lingvodoc.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('buildcss', ['sass:dist']);
+};
