9b6df929d73bc850dbeda59ca0bffee5d93ebacc
Steve Ipatov <ipatov@ispras.ru>
2015-09-18 18:26:47 +0300

    dictionary creation wizard

--- webui/src/js/create_dictionary.js
+++ webui/src/js/create_dictionary.js
@@ -1,4 +1,4 @@
-var app = angular.module('CreateDictionaryModule', ['ui.router', 'ngAnimate', 'ui.bootstrap', 'ngDraggable']);
+var app = angular.module('CreateDictionaryModule', ['ui.router', 'ngAnimate', 'ui.bootstrap']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
@@ -12,12 +12,14 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
         .state('create.step1', {
             url: '/step1',
-            templateUrl: 'createDictionaryStep1.html'
+            templateUrl: 'createDictionaryStep1.html',
+            controller: 'CreateDictionaryController'
         })
 
         .state('create.step2', {
             url: '/step2',
-            templateUrl: 'createDictionaryStep2.html'
+            templateUrl: 'createDictionaryStep2.html',
+            controller: 'CreateDictionaryController'
         })
 
         .state('create.step3', {
@@ -35,56 +37,86 @@ app.controller('CreateDictionaryController', ['$scope', '$http', '$interval', fu
     var languagesUrl = $('#languagesUrl').data('lingvodoc');
     var createLanguageUrl = $('#createLanguageUrl').data('lingvodoc');
 
+    var examplePerspective = {
+        'fields': [{'entity_type': 'protoform', 'data_type': 'text', 'status': 'enabled'},
+            {'entity_type': 'transcription', 'data_type': 'text', 'status': 'enabled'},
+            {'entity_type': 'translation', 'data_type': 'text', 'status': 'enabled'},
+            {
+                'entity_type': 'sound',
+                'data_type': 'sound',
+                'status': 'enabled',
+                'contains': [{'entity_type': 'praat', 'data_type': 'markup', 'status': 'enabled'}]
+            },
+            {'entity_type': 'paradigm_protoform', 'data_type': 'text', 'status': 'enabled', 'group': 'paradigm'},
+            {'entity_type': 'paradigm_transcription', 'data_type': 'text', 'status': 'enabled', 'group': 'paradigm'},
+            {'entity_type': 'paradigm_translation', 'data_type': 'text', 'status': 'enabled', 'group': 'paradigm'},
+            {
+                'entity_type': 'paradigm_sound',
+                'data_type': 'sound',
+                'status': 'enabled',
+                'contains': [{'entity_type': 'paradigm_praat', 'data_type': 'markup', 'status': 'enabled'}]
+            },
+            {'entity_type': 'etymology', 'data_type': 'grouping_tag', 'status': 'enabled'}
+        ],
+        'name': 'perspective_name',
+        'object_id': 'object_id',
+        'client_id': 'client_id'
+    };
+
+
+    var wrapPerspective = function(perspective) {
+
+        for (var i = 0; i < perspective.fields.length; i++) {
+            if (typeof perspective.fields[i].group !== 'undefined') {
+                perspective.fields[i]._groupEnabled = true;
+            }
+
+            if (typeof perspective.fields[i].contains !== 'undefined') {
+                perspective.fields[i]._containsEnabled = true;
+            }
+        }
+
+        return perspective;
+    };
+
+
+    // Data loaded from backend
+    $scope.languages = [];
+    $scope.perspectives = [ wrapPerspective(examplePerspective) ];
+
+
 
     $scope.dictionaryData = {};
-    $scope.draggableObjects = [{name: 'one', type: 'text'}, {name: 'two'}, {name: 'three'}];
+    // current perspective
+    $scope.perspective =  {};
 
 
+    // Event handlers
 
-
-    $scope.fieldTypes = [
-        {
-            'type':  'text',
-            'description': 'Simple text field'
-        },
-        {
-            'type':  'group',
-            'description': 'Groupping field'
-        }
-    ];
-
-
-    $scope.droppedObjects = [];
-
-    $scope.onDropComplete=function(data, evt){
-        console.log('drop success!');
-        var index = $scope.droppedObjects.indexOf(data);
-        if (index == -1) {
-            $scope.droppedObjects.push(data);
+    $scope.enableGroup = function(fieldIndex) {
+        if (typeof $scope.perspective.fields[fieldIndex].group === 'undefined') {
+            $scope.perspective.fields[fieldIndex].group = '';
+        } else {
+            delete $scope.perspective.fields[fieldIndex].group;
         }
     };
 
-    $scope.onDragSuccess=function(data, evt){
-        var index = $scope.droppedObjects.indexOf(data);
-        if (index > -1) {
-            $scope.droppedObjects.splice(index, 1);
+    $scope.enableLinkedField = function(fieldIndex) {
+        if (typeof $scope.perspective.fields[fieldIndex].contains === 'undefined') {
+            console.log('enabled');
+            $scope.perspective.fields[fieldIndex].contains = [{'entity_type': '', 'data_type': 'markup', 'status': 'enabled'}];
+        } else {
+            console.log('disabled');
+            delete $scope.perspective.fields[fieldIndex].contains;
         }
     };
-
-    $scope.onDragStop=function(data, evt){
-
-
-
-    };
-
 
     // Save dictionary
     $scope.saveDictionary = function() {
-        console.log($scope.dictionaryData);
+        console.log($scope.perspective);
     };
 
-
-
+    // Load data from backend
 
     // Load list of languages
     $http.get(languagesUrl).success(function(data, status, headers, config) {
@@ -104,7 +136,33 @@ app.controller('CreateDictionaryController', ['$scope', '$http', '$interval', fu
         // error handling
     });
 
+    $scope.$watch('dictionaryData.perspectiveId', function(id){
+        for (var i = 0; i < $scope.perspectives.length; i++) {
+            if ($scope.perspectives[i].object_id == id) {
+                $scope.perspective = $scope.perspectives[i];
+                break;
+            }
+        }
+    });
 
+
+    // Load list of perspectives
+    //$http.get(languagesUrl).success(function(data, status, headers, config) {
+    //
+    //    $scope.languages = data.languages;
+    //
+    //    // Reload list every 3 seconds
+    //    $interval(function() {
+    //        $http.get(languagesUrl).success(function(data, status, headers, config) {
+    //            $scope.languages = data.languages;
+    //        }).error(function(data, status, headers, config) {
+    //            // error handling
+    //        });
+    //
+    //    }, 3000);
+    //}).error(function(data, status, headers, config) {
+    //    // error handling
+    //});
 
 
 }]);
