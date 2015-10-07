107bc4bb1cd5d20d193d4e321c7075e0d597db55
A. Tapekhin <a.tapekhin@ispras.ru>
2015-10-07 14:56:18 +0300

    alembic fist revision

---
+++ alembic/versions/1bdef094146_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: 1bdef094146
+Revises: 
+Create Date: 2015-10-06 18:53:53.455048
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1bdef094146'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
