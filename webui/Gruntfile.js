0aa0f3544408475b4fde126ebe129cc459c36a8c
Steve Ipatov <ipatov@ispras.ru>
2015-10-25 14:47:57 +0300

    grunt build script fixed

--- webui/Gruntfile.js
+++ webui/Gruntfile.js
@@ -133,6 +133,8 @@ module.exports = function(grunt) {
                     'bower_components/wavesurfer.js/dist/wavesurfer.min.js',
                     'src/js/model.js',
                     'src/js/elan.js',
+                    'src/js/util.js',
+                    'src/js/api.js',
                     'src/js/view_dictionary.js',
                     'src/js/lingvowave.js'
                 ],
