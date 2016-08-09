a112f32aca808bc17cbadfb2723e387ebf324348
A. Tapekhin <a.tapekhin@ispras.ru>
2016-08-09 15:56:43 +0300

    fields view small fix

--- lingvodoc/views/v2/utils.py
+++ lingvodoc/views/v2/utils.py
@@ -395,7 +395,7 @@ def view_field_from_object(request, field):
     response = dict()
     if field and not field.marked_for_deletion:
         response['client_id'] = field.client_id
-        response['client_id'] = field.client_id
+        response['object_id'] = field.object_id
         response['created_at'] = str(field.created_at)
         response['data_type_translation_gist_client_id'] = field.data_type_translation_gist_client_id
         response['data_type_translation_gist_object_id'] = field.data_type_translation_gist_object_id
