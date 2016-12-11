8d6c12e9f392db4d90ca14e3b1fdf715ba532680
st2135 <loed@mail.ru>
2016-12-12 02:15:46 +0300

    maps switching fixed

--- webui/webui/src/main/scala/ru/ispras/lingvodoc/frontend/app/controllers/webui/SociolinguisticsController.scala
+++ webui/webui/src/main/scala/ru/ispras/lingvodoc/frontend/app/controllers/webui/SociolinguisticsController.scala
@@ -171,9 +171,9 @@ class SociolinguisticsController(scope: SociolinguisticsScope, val backend: Back
 
   private[this] def createMap(): LeafletMap = {
     // map object initialization
-    val cssId = "map"
+    val cssId = "sociolinguisticsMap"
     val conf = LeafletMapOptions.zoomControl(true).scrollWheelZoom(true).build
-    val leafletMap = Leaflet.map(cssId, conf) //.setView(Leaflet.latLng(51.505f, -0.09f), 13)
+    val leafletMap = Leaflet.map(cssId, conf)
     val MapId = "lingvodoc_ispras_ru"
     val Attribution = "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>"
 
