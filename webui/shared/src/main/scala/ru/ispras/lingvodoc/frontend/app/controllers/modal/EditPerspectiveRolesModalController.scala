c09423232f40732c7f540affb30a745b823df01f
st2135 <ipatov@ispras.ru>
2016-12-27 21:36:49 +0300

    perspective roles

--- webui/shared/src/main/scala/ru/ispras/lingvodoc/frontend/app/controllers/modal/EditPerspectiveRolesModalController.scala
+++ webui/shared/src/main/scala/ru/ispras/lingvodoc/frontend/app/controllers/modal/EditPerspectiveRolesModalController.scala
@@ -18,8 +18,11 @@ trait EditPerspectiveRolesModalScope extends Scope {
   var dictionary: Dictionary = js.native
   var perspective: Perspective = js.native
   var users: js.Array[UserListEntry] = js.native
+  var searchString: UndefOr[String] = js.native
+  var searchUsers: js.Array[UserListEntry] = js.native
   var error: UndefOr[Throwable] = js.native
   var saveEnabled: Boolean = js.native
+  var permissions: js.Dictionary[js.Dictionary[Boolean]] = js.native
 }
 
 @injectable("EditPerspectiveRolesModalController")
@@ -29,38 +32,31 @@ class EditPerspectiveRolesModalController(scope: EditPerspectiveRolesModalScope,
                                           backend: BackendService,
                                           val timeout: Timeout,
                                           val exceptionHandler: ExceptionHandler,
-                                         params: js.Dictionary[js.Function0[js.Any]])
+                                          params: js.Dictionary[js.Function0[js.Any]])
   extends AbstractController[EditPerspectiveRolesModalScope](scope) with AngularExecutionContextProvider {
 
   private[this] val dictionary = params("dictionary").asInstanceOf[Dictionary]
   private[this] val perspective = params("perspective").asInstanceOf[Perspective]
   private[this] var permissions = Map[String, Seq[UserListEntry]]()
-  private[this] var addUsersActive = Seq[String]()
+  //private[this] var addUsersActive = Seq[String]()
+  private[this] var allUsers = Seq[UserListEntry]()
 
   scope.dictionary = dictionary
   scope.perspective = perspective
   scope.users = js.Array[UserListEntry]()
+  scope.searchString = Option.empty[String].orUndefined
+  scope.searchUsers = js.Array[UserListEntry]()
   scope.saveEnabled = true
 
   load()
 
   @JSExport
-  def getRoles(): js.Array[String] = {
-    permissions.keys.toJSArray
-  }
-
-  @JSExport
-  def getUsers(roleName: String): js.Array[UserListEntry] = {
-    permissions.find(_._1 == roleName) match {
-      case Some(e) => e._2.toJSArray
-      case None => js.Array[UserListEntry]()
-    }
-  }
-
-  @JSExport
   def ok() = {
-    val roles = perspectiveRoles()
     scope.saveEnabled = false
+    val permissions = scope.permissions.toMap map { case (role, e) =>
+      role -> e.toMap.filter(_._2).keys.map(_.toInt).toSeq
+    }
+    val roles = PerspectiveRoles(permissions, Map.empty[String, Seq[Int]])
     backend.setDPerspectiveRoles(CompositeId.fromObject(dictionary), CompositeId.fromObject(perspective), roles) onComplete {
       case Success(_) => instance.close(())
       case Failure(e) => setError(e)
@@ -73,73 +69,51 @@ class EditPerspectiveRolesModalController(scope: EditPerspectiveRolesModalScope,
   }
 
   @JSExport
-  def toggleAddUsers(role: String) = {
-    if (!isAddUsersActive(role)) {
-      addUsersActive = addUsersActive :+ role
+  def getUsers(): js.Array[UserListEntry] = {
+    if (!js.isUndefined(scope.permissions)) {
+      val ids = scope.permissions.headOption match {
+        case Some(p) => p._2.keys.map(_.toInt).toSeq
+        case None => Seq.empty[Int]
+      }
+      ids.flatMap(id => allUsers.find(_.id == id)).toJSArray
     } else {
-      addUsersActive = addUsersActive.filterNot(_ == role)
+      Seq.empty[UserListEntry].toJSArray
     }
   }
 
   @JSExport
-  def isAddUsersActive(role: String): Boolean = {
-    addUsersActive.contains(role)
-  }
-
-
-  @JSExport
-  def userHasRole(user: UserListEntry, role: String): Boolean = {
-    permissions.keySet.find(_ == role) match {
-      case Some(_) => permissions(role).exists(_.id == user.id)
-      case None => false
+  def searchUser() = {
+    scope.searchString.toOption foreach { q =>
+      scope.searchUsers = allUsers.filterNot(u => scope.users.exists(_.id == u.id))
+        .filter(user => user.login.toLowerCase.contains(q.toLowerCase) || user.intlName.toLowerCase.contains(q.toLowerCase)).take(5).toJSArray
     }
   }
 
   @JSExport
-  def addRole(user: UserListEntry, role: String) = {
-    permissions.keySet.find(_ == role).foreach {
-      _ =>
-        permissions = permissions + (role -> (permissions(role) :+ user))
-    }
-  }
-
-
-  @JSExport
-  def removeRole(user: UserListEntry, role: String) = {
-    permissions.keySet.find(_ == role).foreach {
-      _ =>
-        permissions = permissions + (role -> permissions(role).filterNot(_.id == user.id))
-    }
-  }
-
-
-  private[this] def getPermissions(users: Seq[UserListEntry], roles: PerspectiveRoles): Map[String, Seq[UserListEntry]] = {
-    roles.users.map { case (roleName, ids) =>
-      roleName -> ids.flatMap(userId => users.find(_.id == userId))
-    }
-  }
-
-  private[this] def perspectiveRoles(): PerspectiveRoles = {
-    val users = permissions.map { case (role, u) => role -> u.map(_.id)}
-    PerspectiveRoles(users, Map[String, Seq[Int]]())
+  def addUser(searchUser: UserListEntry) = {
+    scope.permissions = scope.permissions.toMap.map { case (role, users) =>
+      role -> (users.toMap + (searchUser.id.toString -> false)).toJSDictionary
+    }.toJSDictionary
   }
 
   private[this] def setError(e: Throwable) = {
     scope.error = e
   }
 
+  private[this] def createRoles(users: Seq[UserListEntry], roles: PerspectiveRoles) = {
+    val activeUsers = roles.users.values.reduce(_ ++ _).distinct.sortWith(_ > _).flatMap(id => users.find(_.id == id))
+    roles.users.map { case (role, roleUsers) =>
+      role -> activeUsers.map(u => (u.id.toString, roleUsers.contains(u.id))).toMap.toJSDictionary
+    }.toJSDictionary
+  }
 
   private[this] def load() = {
-
-    backend.getUsers onComplete {
-      case Success(users) =>
-        scope.users = users.toJSArray
-        backend.getPerspectiveRoles(CompositeId.fromObject(dictionary), CompositeId.fromObject(perspective)) onComplete {
-          case Success(roles) =>
-            permissions = getPermissions(users, roles)
-          case Failure(e) =>
-        }
-      case Failure(e) =>
+    backend.getUsers flatMap { users =>
+      allUsers = users
+      backend.getPerspectiveRoles(CompositeId.fromObject(dictionary), CompositeId.fromObject(perspective)) map { roles =>
+        scope.users = roles.users.values.reduce(_ ++ _).distinct.sortWith(_ > _).flatMap(id => users.find(_.id == id)).toJSArray
+        scope.permissions = createRoles(users, roles)
+      }
     }
   }
 }
