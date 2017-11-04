97c6c0b85f5f7099a65794729e99fa93ae0a43ad
IgorBMSTU <igor95n@gmail.com>
2017-11-04 04:57:40 +0300

    metadata fix from 572d4f9e05663465c19c3af07a97c76c0f82129f commit added

--- lingvodoc/views/v2/views.py
+++ lingvodoc/views/v2/views.py
@@ -264,6 +264,39 @@ from lingvodoc.models import UserRequest as dbUserRequest
 def testing(request):
     # Hello, testing, my old friend
     # I've come to use you once again
+    simpler_info = lambda x: [i['info']['content'] for i in x]
+    persp_to_dict = lambda x: [
+        p.additional_metadata['location']['content'] for p in x]
+    persps = DBSession.query(DictionaryPerspective).filter().all()
+    dicts = []
+    for persp in persps:
+        if not persp.additional_metadata:
+            continue
+        parent = persp.parent
+        if not parent.additional_metadata:
+            parent.additional_metadata = dict()
+        if not parent.additional_metadata.get('location') and persp.additional_metadata.get('location'):
+            parent.additional_metadata['location'] = persp.additional_metadata['location']['content']
+        if persp.additional_metadata.get('location'):
+            del persp.additional_metadata['location']
+        if not parent.additional_metadata.get('authors') and persp.additional_metadata.get('authors'):
+            parent.additional_metadata['authors'] = persp.additional_metadata['authors']['content']
+        if persp.additional_metadata.get('authors'):
+            del persp.additional_metadata['authors']
+        if persp.additional_metadata.get('info'):
+            if not parent.additional_metadata.get('blobs'):
+                parent.additional_metadata['blobs'] = list()
+            for item in simpler_info(persp.additional_metadata['info']['content']):
+                if item not in parent.additional_metadata['blobs']:
+                    parent.additional_metadata['blobs'].append(item)
+            del persp.additional_metadata['info']
+        if persp.additional_metadata.get('origin_client_id') and persp.additional_metadata.get('origin_object_id'):
+            persp.additional_metadata['origin_id'] = (persp.additional_metadata['origin_client_id'], persp.additional_metadata['origin_object_id'])
+            del persp.additional_metadata['origin_client_id']
+            del persp.additional_metadata['origin_object_id']
+        flag_modified(parent, 'additional_metadata')
+        flag_modified(persp, 'additional_metadata')
+    # and again
     add_dict_to_grant = DBSession.query(dbUserRequest).filter_by(type="add_dict_to_grant").all()
     for req in add_dict_to_grant:
         subject = req.subject
