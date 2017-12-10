82ce01b07017bac7ac92b74333137063704ef44c
Ivan Beloborodov <ivbel@ispras.ru>
2017-12-10 23:25:11 +0300

    install with both old and new interfaces

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 import os
 
 from setuptools import setup, find_packages
+
+from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 
@@ -12,7 +14,66 @@ with open(os.path.join(here, 'CHANGES.md')) as f:
     CHANGES = f.read()
 
 
-class install_with_interface(install):
+class React_Interface_Mixin(object):
+    """
+    Adds ability to set up React-based interface to setuptools commands.
+    """
+
+    def initialize_options(self):
+        """
+        Initializes standard setuptools' command options and a React interface option.
+        """
+
+        super().initialize_options()
+        self.react_interface = None
+
+    def setup_react_interface(self, lingvodoc_dir):
+        """
+        Sets up React-based interface by copying its distribution files to appropriate locations.
+        """
+
+        self.announce('Installing React-based interface')
+
+        interface_dir = os.path.expanduser(self.react_interface)
+
+        index_from_path = os.path.join(interface_dir, 'index.html')
+
+        index_to_path = os.path.join(lingvodoc_dir,
+            'views', 'v2', 'templates', 'new_interface.pt')
+
+        if os.path.exists(index_to_path):
+            os.remove(index_to_path)
+
+        self.copy_file(index_from_path, index_to_path)
+
+        assets_dir = os.path.join(interface_dir, 'assets')
+
+        self.copy_tree(assets_dir, os.path.join(
+            lingvodoc_dir, 'assets'))
+
+
+class develop_with_interface(React_Interface_Mixin, develop):
+    """
+    Extends 'develop' setup.py command with ability to develop React-based interface.
+    """
+
+    user_options = develop.user_options + [
+        ('react-interface=', None, 'path to React-based interface distribution')]
+
+    def run(self):
+        """
+        Runs development installation, sets up React-based interface if required.
+        """
+
+        develop.run(self)
+
+        if self.react_interface is not None:
+
+            self.setup_react_interface(
+                os.path.join(self.egg_path, 'lingvodoc'))
+
+
+class install_with_interface(React_Interface_Mixin, install):
     """
     Extends 'install' setup.py command with ability to install React-based interface.
     """
@@ -20,36 +81,17 @@ class install_with_interface(install):
     user_options = install.user_options + [
         ('react-interface=', None, 'path to React-based interface distribution')]
 
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.react_interface = None
-
     def run(self):
+        """
+        Performs installation, then installs React-based interface if required.
+        """
+
         install.run(self)
 
-        # If we have been given a path to the React-based interface, we install it too.
-
         if self.react_interface is not None:
-            self.announce('Installing React-based interface')
 
-            interface_dir = self.react_interface
-            self.ensure_dirname('react_interface')
-
-            install_dir = os.path.join(self.install_lib, 'lingvodoc')
-
-            index_path = os.path.join(interface_dir, 'index.html')
-            main_pt_path = os.path.join(
-                install_dir, 'views', 'v2', 'templates', 'main.pt')
-
-            if os.path.exists(main_pt_path):
-                os.remove(main_pt_path)
-
-            self.copy_file(index_path, main_pt_path)
-
-            assets_dir = os.path.join(interface_dir, 'assets')
-
-            self.copy_tree(assets_dir, os.path.join(
-                install_dir, 'assets'))
+            self.setup_react_interface(
+                os.path.join(self.install_lib, 'lingvodoc'))
 
 
 requires = [
@@ -81,6 +123,8 @@ setup(name='lingvodoc',
       [console_scripts]
       initialize_lingvodoc_db = lingvodoc.scripts.initializedb:main
       """,
-      cmdclass={'install': install_with_interface},
+      cmdclass={
+          'develop': develop_with_interface,
+          'install': install_with_interface},
       )
 
