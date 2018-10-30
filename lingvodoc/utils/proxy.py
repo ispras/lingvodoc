94695afe7493414558ae942f8b79802f5b5830ea
IgorBMSTU <igor95n@gmail.com>
2018-10-30 21:05:46 +0300

    proxy version fixes

--- lingvodoc/utils/proxy.py
+++ lingvodoc/utils/proxy.py
@@ -14,10 +14,31 @@ class ProxyPass(Exception):
         else:
             cookies = None
         session = requests.Session()
+        #session.headers.update(**request.headers)
         session.headers.update({'Connection': 'Keep-Alive'})
+        session.headers.update({'Content-type': 'application/json'})
+        if cookies:
+            session.headers.update(
+                {
+                    "Cookie": "auth_tkt=%s; locale_id=%s; client_id=%s" % (cookies["auth_tkt"],
+                                                                                            cookies["locale_id"],
+                                                                                            cookies["client_id"])
+                })
         adapter = requests.adapters.HTTPAdapter(pool_connections=1, pool_maxsize=1, max_retries=10)
         session.mount('http://', adapter)
-        status = session.post(path, data=request.body, cookies=cookies, headers=request.headers)
+        # session.cookies.set(cookies)
+
+        #session.cookies.clear()
+        if cookies:
+            session.cookies.set("auth_tkt", cookies["auth_tkt"])
+            session.cookies.set("locale_id", str(cookies["locale_id"]))
+            session.cookies.set("client_id", str(cookies["client_id"]))
+        #session.cookies.set("server_cookies", request.cookies.get('server_cookies'))
+        # for key in cookies:
+        #     session.cookies.set(key, cookies[key])
+        status = session.post(path, data=request.body, headers=session.headers, cookies=cookies)
+        #print(status.request)
+
         self.response_body = status.content
 
 
