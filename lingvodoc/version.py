0b8633c2a93571a0808215b3509ba7fd38263ddd
Ivan Beloborodov <ivbel@ispras.ru>
2021-03-14 02:40:40 +0300

    PostgreSQL 13, Ubuntu 20.04 backend environment #447

--- lingvodoc/version.py
+++ lingvodoc/version.py
@@ -1,5 +1,5 @@
 
 # This file is overwritten on setup.
 # Please do not modify it and ignore its changes in version control.
-__version__ = '2.1.1+1340-2ed13ecb-2021.03.11-11:32+modified-2021.03.13-19:04'
+__version__ = None
 
