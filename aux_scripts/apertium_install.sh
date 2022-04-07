da1c950b1953c807869644cf4b9086537d5b9401
Ivan Beloborodov <ivbel@ispras.ru>
2022-04-07 12:39:50 +0300

    apertium_install.sh with nightly

--- aux_scripts/apertium_install.sh
+++ aux_scripts/apertium_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ $# -eq 0 ]; then
     echo -e "Needs one parameter: the path to the directory where apertium packages should be installed ";
     exit 1;
@@ -7,22 +8,34 @@ if ! [ -d "$1" ]; then
     echo -e "Passed parameter is not a valid path. ";
     exit 1;
 fi;
+
 locale-gen en_US.UTF-8 && update-locale;
-apt install -y lttoolbox apertium-dev \
-apertium-lex-tools hfst libhfst-dev cg3 cg3-dev autoconf;
-if ! [ -d "$1/apertium-kaz" ]; then git clone https://github.com/apertium/apertium-kaz $1/apertium-kaz; fi;
-pushd $1/apertium-kaz && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-tat" ]; then git clone https://github.com/apertium/apertium-tat $1/apertium-tat; fi;
-pushd $1/apertium-tat && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-rus" ]; then git clone https://github.com/apertium/apertium-rus $1/apertium-rus; fi;
-pushd $1/apertium-rus && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-kaz-rus" ]; then git clone https://github.com/apertium/apertium-kaz-rus $1/apertium-kaz-rus; fi;
-pushd $1/apertium-kaz-rus && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-tat-rus" ]; then git clone https://github.com/apertium/apertium-tat-rus $1/apertium-tat-rus; fi;
-pushd $1/apertium-tat-rus && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-sah" ]; then git clone https://github.com/apertium/apertium-sah $1/apertium-sah; fi;
-pushd $1/apertium-sah && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-bak" ]; then git clone https://github.com/apertium/apertium-bak $1/apertium-bak; fi;
-pushd $1/apertium-bak && ./autogen.sh && ./configure && make && make install && popd;
-if ! [ -d "$1/apertium-tat-bak" ]; then git clone https://github.com/apertium/apertium-tat-bak $1/apertium-tat-bak; fi;
-pushd $1/apertium-tat-bak && ./autogen.sh && ./configure && make && make install && popd;
+curl -sS https://apertium.projectjj.com/apt/install-nightly.sh | bash;
+apt install -y lttoolbox apertium-dev apertium-lex-tools apertium-separable hfst libhfst-dev cg3 cg3-dev autoconf;
+
+if [ $# -gt 1 ]; then
+
+# Assuming we got a list of specific parsers to update.
+
+PARSER_LIST=("${@:2}");
+
+else
+
+# Updating all parsers.
+
+PARSER_LIST=("apertium-kaz" "apertium-tat" "apertium-rus" "apertium-kaz-rus" "apertium-tat-rus" "apertium-sah" "apertium-bak" "apertium-tat-bak");
+
+fi;
+
+for PARSER_NAME in "${PARSER_LIST[@]}"; do
+
+  if ! [ -d "$1/$PARSER_NAME" ]; then
+    git clone https://github.com/apertium/$PARSER_NAME $1/$PARSER_NAME;
+  else
+    pushd $1/$PARSER_NAME && git pull && popd;
+  fi;
+
+  pushd $1/$PARSER_NAME && ./autogen.sh && ./configure && make && make install && popd;
+
+done;
+
