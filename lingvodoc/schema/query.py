5a2936189c1119d145d0170b050d0094db3d8d48
Vladimir Monakhov <vmonakhov@ispras.ru>
2023-06-16 15:56:41 +0300

    Fixes

--- lingvodoc/schema/query.py
+++ lingvodoc/schema/query.py
@@ -4490,8 +4490,8 @@ class Query(graphene.ObjectType):
         # Check if the current user is administrator
         client_id = info.context.get('client_id')
         user_id = DBSession.query(Client.user_id).filter_by(id=client_id).scalar()
-        if int(user_id) != 1:
-            return "Only administrator can fill in logs using request"
+        if user_id != 1:
+            return ResponseError("Only administrator can fill in logs using request.")
 
         period = 300
         step = 0.25
