6d4a1ad6b22c4325f8c77ca3c9ebf493c581ad31
Vladimir Monakhov <vmonakhov@ispras.ru>
2023-07-23 18:47:24 +0300

    Fix

--- lingvodoc/utils/creation.py
+++ lingvodoc/utils/creation.py
@@ -923,11 +923,11 @@ def create_field(translation_atoms, client_id=66):
                                                   [client_id, None],
                                                   gist_type="Field")
 
-    data_type_translation_gist_id = translation_gist_search('Text')
+    data_type_translation_gist = translation_gist_search('Text')
     dbfield = Field(client_id=client_id,
                     object_id=None,
-                    data_type_translation_gist_client_id=data_type_translation_gist_id[0],
-                    data_type_translation_gist_object_id=data_type_translation_gist_id[1],
+                    data_type_translation_gist_client_id=data_type_translation_gist.client_id,
+                    data_type_translation_gist_object_id=data_type_translation_gist.object_id,
                     translation_gist_client_id=translation_gist_id[0],
                     translation_gist_object_id=translation_gist_id[1],
                     marked_for_deletion=False)
