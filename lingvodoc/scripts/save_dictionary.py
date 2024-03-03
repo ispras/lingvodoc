dbe7b6e38f9a3233c71d77c824d0c16f55d9bd9f
vmonakhov <vmonakhov@ispras.ru>
2024-03-04 01:15:32 +0300

    Refactoring

--- lingvodoc/scripts/save_dictionary.py
+++ lingvodoc/scripts/save_dictionary.py
@@ -163,14 +163,11 @@ def find_group_by_tags(
 
             .filter(
                 LexicalEntry.marked_for_deletion == False,
-                Entity.parent_client_id == LexicalEntry.client_id,
-                Entity.parent_object_id == LexicalEntry.object_id,
-                Entity.field_client_id == field_client_id,
-                Entity.field_object_id == field_object_id,
+                Entity.parent_id == LexicalEntry.id,
+                Entity.field_id == (field_client_id, field_object_id),
                 Entity.marked_for_deletion == False,
                 Entity.content.in_(tag_list),
-                PublishingEntity.client_id == Entity.client_id,
-                PublishingEntity.object_id == Entity.object_id,
+                PublishingEntity.id == Entity.id,
                 PublishingEntity.accepted == True))
 
         if published is not None:
@@ -200,11 +197,9 @@ def find_group_by_tags(
             .filter(
                 tuple_(Entity.parent_client_id, Entity.parent_object_id)
                     .in_(entry_id_list),
-                Entity.field_client_id == field_client_id,
-                Entity.field_object_id == field_object_id,
+                Entity.field_id == (field_client_id, field_object_id),
                 Entity.marked_for_deletion == False,
-                PublishingEntity.client_id == Entity.client_id,
-                PublishingEntity.object_id == Entity.object_id,
+                PublishingEntity.id == Entity.id,
                 PublishingEntity.accepted == True))
 
         if published is not None:
@@ -253,8 +248,7 @@ def generate_massive_cell_simple(
 
     for lex in lexical_entries:
         entities = session.query(Entity).join(PublishingEntity) \
-            .filter(Entity.parent_client_id == lex.client_id,
-                    Entity.parent_object_id == lex.object_id,
+            .filter(Entity.parent_id == (lex.client_id, lex.object_id),
                     PublishingEntity.accepted == True,
                     Entity.marked_for_deletion == False)
         if published is not None:
@@ -296,8 +290,7 @@ def generate_massive_cell_optimized(
                 tuple_(Entity.field_client_id, Entity.field_object_id)
                     .in_(text_fields),
                 Entity.content != None,
-                PublishingEntity.client_id == Entity.client_id,
-                PublishingEntity.object_id == Entity.object_id,
+                PublishingEntity.id == Entity.id,
                 PublishingEntity.accepted == True)
 
             .add_columns(
@@ -336,14 +329,12 @@ def generate_massive_cell_cte(
 
         .filter(
             LexicalEntry.marked_for_deletion == False,
-            Entity.parent_client_id == LexicalEntry.client_id,
-            Entity.parent_object_id == LexicalEntry.object_id,
+            Entity.parent_id == LexicalEntry.id,
             Entity.field_client_id == 66,
             Entity.field_object_id == 25,
             Entity.marked_for_deletion == False,
             Entity.content == tag,
-            PublishingEntity.client_id == Entity.client_id,
-            PublishingEntity.object_id == Entity.object_id,
+            PublishingEntity.id == Entity.id,
             PublishingEntity.accepted == True))
 
     if published is not None:
@@ -371,22 +362,18 @@ def generate_massive_cell_cte(
         
         .filter(
             LexicalEntry.marked_for_deletion == False,
-            E_entry.parent_client_id == LexicalEntry.client_id,
-            E_entry.parent_object_id == LexicalEntry.object_id,
+            E_entry.parent_id == LexicalEntry.id,
             E_entry.field_client_id == 66,
             E_entry.field_object_id == 25,
             E_entry.marked_for_deletion == False,
-            P_entry.client_id == E_entry.client_id,
-            P_entry.object_id == E_entry.object_id,
+            P_entry.id == E_entry.id,
             P_entry.accepted == True,
             E_entry.content == E_tag.content,
-            E_tag.parent_client_id == cte_query.c.client_id,
-            E_tag.parent_object_id == cte_query.c.object_id,
+            E_tag.parent_id == (cte_query.c.client_id, cte_query.c.object_id),
             E_tag.field_client_id == 66,
             E_tag.field_object_id == 25,
             E_tag.marked_for_deletion == False,
-            P_tag.client_id == E_tag.client_id,
-            P_tag.object_id == E_tag.object_id,
+            P_tag.id == E_tag.id,
             P_tag.accepted == True))
 
     if published is not None:
@@ -415,8 +402,7 @@ def generate_massive_cell_cte(
                 tuple_(Entity.field_client_id, Entity.field_object_id)
                     .in_(text_fields),
                 Entity.content != None,
-                PublishingEntity.client_id == Entity.client_id,
-                PublishingEntity.object_id == Entity.object_id,
+                PublishingEntity.id == Entity.id,
                 PublishingEntity.accepted == True)
 
             .add_columns(
@@ -674,8 +660,7 @@ def generate_massive_cell_temp_table(
                         'select * from ' + text_field_id_table_name)),
 
                 Entity.content != None,
-                PublishingEntity.client_id == Entity.client_id,
-                PublishingEntity.object_id == Entity.object_id,
+                PublishingEntity.id == Entity.id,
                 PublishingEntity.accepted == True)
 
             .add_columns(
@@ -1534,8 +1519,7 @@ class Save_Context(object):
             None if not self.cognates_flag else
 
             self.session.query(DictionaryPerspectiveToField).filter_by(
-                parent_client_id=perspective.client_id,
-                parent_object_id=perspective.object_id,
+                parent_id=(perspective.client_id, perspective.object_id),
                 marked_for_deletion=False,
                 field_client_id=66,
                 field_object_id=25).first())
@@ -1710,8 +1694,7 @@ class Save_Context(object):
 
                     DictionaryPerspective.marked_for_deletion == False,
 
-                    Dictionary.client_id == DictionaryPerspective.parent_client_id,
-                    Dictionary.object_id == DictionaryPerspective.parent_object_id,
+                    Dictionary.id == DictionaryPerspective.parent_id,
                     Dictionary.marked_for_deletion == False))
 
         # For each perspective we compile ordering info and its title, with ordering based on standard
@@ -1757,15 +1740,12 @@ class Save_Context(object):
                             .label('name'))
 
                     .filter(
-                        DictionaryPerspectiveToField.parent_client_id == perspective.client_id,
-                        DictionaryPerspectiveToField.parent_object_id == perspective.object_id,
+                        DictionaryPerspectiveToField.parent_id == (perspective.client_id, perspective.object_id),
                         DictionaryPerspectiveToField.marked_for_deletion == False,
-                        Field.client_id == DictionaryPerspectiveToField.field_client_id,
-                        Field.object_id == DictionaryPerspectiveToField.field_object_id,
+                        Field.id == DictionaryPerspectiveToField.field_id,
                         Field.marked_for_deletion == False,
                         self.field_condition,
-                        Field.translation_gist_client_id == TranslationAtom.parent_client_id,
-                        Field.translation_gist_object_id == TranslationAtom.parent_object_id,
+                        Field.translation_gist_id == TranslationAtom.parent_id,
                         TranslationAtom.marked_for_deletion == False)
 
                     .group_by(
@@ -1788,8 +1768,7 @@ class Save_Context(object):
                         DictionaryPerspectiveToField.position)
 
                     .filter(
-                        DictionaryPerspectiveToField.parent_client_id == perspective.client_id,
-                        DictionaryPerspectiveToField.parent_object_id == perspective.object_id,
+                        DictionaryPerspectiveToField.parent_id == (perspective.client_id, perspective.object_id),
                         DictionaryPerspectiveToField.marked_for_deletion == False,
                         DictionaryPerspectiveToField.field_id.in_(
                             sqlalchemy.text('select * from text_field_id_view')))
@@ -2058,18 +2037,13 @@ class Save_Context(object):
                                             literal(1))
 
                                         .filter(
-                                            Sound.parent_client_id == perspective.client_id,
-                                            Sound.parent_object_id == perspective.object_id,
+                                            Sound.parent_id == (perspective.client_id, perspective.object_id),
                                             Sound.marked_for_deletion == False,
-                                            Sound.field_client_id == sound_fid[0],
-                                            Sound.field_object_id == sound_fid[1],
-                                            Markup.parent_client_id == perspective.client_id,
-                                            Markup.parent_object_id == perspective.object_id,
+                                            Sound.field_id == sound_fid,
+                                            Markup.parent_id == (perspective.client_id, perspective.object_id),
                                             Markup.marked_for_deletion == False,
-                                            Markup.field_client_id == markup_fid[0],
-                                            Markup.field_object_id == markup_fid[1],
-                                            Markup.self_client_id == Sound.client_id,
-                                            Markup.self_object_id == Sound.object_id)
+                                            Markup.field_id == markup_fid,
+                                            Markup.self_id == Sound.id)
 
                                         .exists())
 
@@ -2339,8 +2313,7 @@ class Save_Context(object):
 
         entities = (
             self.session.query(Entity).filter(
-                Entity.parent_client_id == entry.client_id,
-                Entity.parent_object_id == entry.object_id,
+                Entity.parent_id == (entry.client_id, entry.object_id),
                 Entity.marked_for_deletion == False))
 
         if published is not None or accepted is not None:
@@ -2872,11 +2845,10 @@ def compile_document(
     Compiles analysis results into xlsx/docx/rtf document.
     """
 
-    dictionary = session.query(Dictionary).filter_by(client_id=client_id, object_id=object_id,
+    dictionary = session.query(Dictionary).filter_by(id=(client_id, object_id),
                                                      marked_for_deletion=False).one()
 
-    perspectives = session.query(DictionaryPerspective).filter_by(parent_client_id=client_id,
-                                                                  parent_object_id=object_id,
+    perspectives = session.query(DictionaryPerspective).filter_by(parent_id=(client_id, object_id),
                                                                   marked_for_deletion=False).all()
 
     # Processing all perspectives of the dictionary.
@@ -2888,8 +2860,7 @@ def compile_document(
             __debug_flag__ = __debug_flag__)
 
         lexical_entries = session.query(LexicalEntry, Entity).join(Entity).join(PublishingEntity) \
-            .filter(LexicalEntry.parent_client_id == perspective.client_id,
-                    LexicalEntry.parent_object_id == perspective.object_id,
+            .filter(LexicalEntry.parent_id == (perspective.client_id, perspective.object_id),
                     LexicalEntry.marked_for_deletion == False,
                     Entity.marked_for_deletion == False,
                     PublishingEntity.accepted == True)
@@ -3107,7 +3078,7 @@ def save_dictionary(
 
         else:
 
-            dictionary = session.query(Dictionary).filter_by(client_id=client_id, object_id=object_id).one()
+            dictionary = session.query(Dictionary).filter_by(id=(client_id, object_id)).one()
 
             dict_status_atom = session.query(TranslationAtom).filter_by(
                 parent_client_id=dictionary.state_translation_gist_client_id,
