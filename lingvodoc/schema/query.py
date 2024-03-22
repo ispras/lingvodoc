de4ce0a4d3c6d7a1d020129cab5ec4c10b1e84f6
vmonakhov <vmonakhov@ispras.ru>
2024-03-22 18:33:07 +0300

    Field reordering -- https://github.com/ispras/lingvodoc-react/issues/1109 (#1500)
    * reorder columns
    
    * auto reordering
    
    * fix
    
    * comments

--- lingvodoc/schema/query.py
+++ lingvodoc/schema/query.py
@@ -60,7 +60,8 @@ from sqlalchemy import (
     func,
     or_,
     tuple_,
-    union)
+    union,
+    desc)
 
 import sqlalchemy.dialects.postgresql as postgresql
 
@@ -344,6 +345,7 @@ from lingvodoc.views.v2.utils import (
     storage_file,
     view_field_from_object)
 
+from pdb import set_trace as A
 
 # Setting up logging.
 log = logging.getLogger(__name__)
@@ -7042,6 +7044,104 @@ class BidirectionalLinks(graphene.Mutation):
                     'Exception:\n' + traceback_string))
 
 
+class ReorderColumns(graphene.Mutation):
+    """
+    curl 'https://lingvodoc.ispras.ru/api/graphql' \
+    -H 'Content-Type: application/json' \
+    -H 'Cookie: locale_id=2; auth_tkt=$TOKEN!userid_type:int; client_id=$ID' \
+    --data-raw '{ "operationName": "reorder_columns", "variables": {"perspectiveId": null}, \
+    "query": "mutation reorder_columns($perspectiveId: LingvodocID) \
+    { reorder_columns(perspective_id: $perspectiveId, debug_flag: true) { triumph }}"}'
+
+    #! set perspectiveId to concrete LingvodocID for one perspective's positions reordering,
+    #! otherwise all perspectives with duplicated columns positions will be processed
+    """
+
+    class Arguments:
+
+        perspective_id = LingvodocID()
+        debug_flag = graphene.Boolean()
+
+    triumph = graphene.Boolean()
+
+    @staticmethod
+    def mutate(
+        root,
+        info,
+        perspective_id = None,
+        debug_flag = False):
+
+        try:
+            client_id = info.context.client_id
+            if debug_flag:
+                log.debug(f"client_id: {client_id}")
+
+            client = DBSession.query(Client).filter_by(id = client_id).first()
+
+            if not client or client.user_id != 1:
+                return ResponseError('Only administrator can reorder columns.')
+
+            perspective_list = [perspective_id] if perspective_id else (
+                DBSession
+                    .query(
+                        dbColumn.parent_client_id,
+                        dbColumn.parent_object_id)
+                    .filter_by(
+                        marked_for_deletion= 'false')
+                    .group_by(
+                        dbColumn.parent_client_id,
+                        dbColumn.parent_object_id)
+                    .having(
+                        func.count(dbColumn.position) -
+                        func.count(func.distinct(dbColumn.position)) > 0)
+                    .all()) or []
+
+            for per_id in perspective_list:
+                column_list = (
+                    DBSession
+                        .query(dbColumn)
+                        .filter_by(
+                            parent_id = per_id,
+                            marked_for_deletion = 'false')
+                        .order_by(
+                            dbColumn.position,
+                            dbColumn.client_id,
+                            dbColumn.object_id,
+                            dbColumn.created_at)
+                        .all()) or []
+
+                if debug_flag:
+                    log.debug(f'Processing {per_id} ...')
+
+                for i in range(1, len(column_list)):
+                    if column_list[i].position <= column_list[i-1].position:
+                        column_list[i].position = column_list[i-1].position + 1
+
+                        if debug_flag:
+                            log.debug(f">> Changed {i}'th")
+
+            if debug_flag:
+                log.debug(f"Total processed {len(perspective_list)} perspectives.")
+
+            return ReorderColumns(triumph = True)
+
+        except Exception as exception:
+
+            traceback_string = (
+
+                ''.join(
+                    traceback.format_exception(
+                        exception, exception, exception.__traceback__))[:-1])
+
+            log.warning('columns_reordering: exception')
+            log.warning(traceback_string)
+
+            return (
+
+                ResponseError(
+                    'Exception:\n' + traceback_string))
+
+
 class MyMutations(graphene.ObjectType):
     """
     Mutation classes.
@@ -7109,6 +7209,7 @@ class MyMutations(graphene.ObjectType):
     create_column = CreateColumn.Field()
     update_column = UpdateColumn.Field()
     delete_column = DeleteColumn.Field()
+    reorder_columns = ReorderColumns.Field()
     create_grant = CreateGrant.Field()
     update_grant = UpdateGrant.Field()
     # delete_grant = DeleteGrant.Field()
