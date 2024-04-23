dffaa62435ac44525f21bf935544c3127b5a3301
vmonakhov <vmonakhov@ispras.ru>
2024-04-23 23:22:15 +0300

    next

--- lingvodoc/views/v2/shimmer.py
+++ lingvodoc/views/v2/shimmer.py
@@ -3,10 +3,24 @@ from jitter import unidirectional_autowindow, get_window_points
 from math import pi
 
 
+class AmplitudeTier:
+    def __init__(self, tmin, tmax):
+        self.tmin = tmin
+        self.tmax = tmax
+        self.times = []
+        self.values = []
+
+    def addPoint(self, t, value):
+        self.times.append(t)
+        self.values.append(value)
+
+
 def get_hann_windowed_rms(sound, tmid, widthLeft, widthRight):
-    imin, imax = get_window_samples(sound, tmid - widthLeft, tmid + widthRight)
-    if imin is None or imax is None or imax - imin < 2:
+
+    if (edges := get_window_samples(sound, tmid - widthLeft, tmid + widthRight)) is None:
         return None
+
+    imin, imax = edges
 
     sumOfSquares = 0.0
     windowSumOfSquares = 0.0
@@ -30,7 +44,7 @@ def get_window_samples(sound, tmin, tmax):
     imax = np.floor((tmax - sound['x1']) / sound['dx'])
     imin = int(max(0.0, imin))
     imax = int(min(sound['nx'], imax))
-    if imin > imax:
+    if imax - imin < 2:
         return None
     return imin, imax
 
@@ -38,39 +52,23 @@ def get_window_samples(sound, tmin, tmax):
 def point_to_amplitude_period(pulse, sound, tmin, tmax, pmin, pmax, maximumPeriodFactor):
     try:
         tmin, tmax = unidirectional_autowindow(pulse, tmin, tmax)
-        peaks = get_window_points(pulse, tmin, tmax)
-        if len(peaks) < 3:
+        left, right = get_window_points(pulse, tmin, tmax)
+        if right - left < 2:
             raise ValueError(f"Too few pulses between {tmin} and {tmax} seconds.")
 
-        him = AmplitudeTier_create(tmin, tmax)
-        for i in range(1, len(peaks) - 1):
-            p1 = pulse.t[i] - pulse.t[i - 1]
-            p2 = pulse.t[i + 1] - pulse.t[i]
+        amplitude = AmplitudeTier(tmin, tmax)
+        for i in range(1, right - left):
+            p1 = pulse['t'][i] - pulse['t'][i - 1]
+            p2 = pulse['t'][i + 1] - pulse['t'][i]
             intervalFactor = p1 / p2 if p1 > p2 else p2 / p1
             if pmin == pmax or (pmin <= p1 <= pmax and pmin <= p2 <= pmax and intervalFactor <= maximumPeriodFactor):
-                peak = get_hann_windowed_rms(sound, pulse.t[i], 0.2 * p1, 0.2 * p2)
-                if np.isfinite(peak) and peak > 0.0:
-                    him.addPoint(pulse.t[i], peak)
+                peak = get_hann_windowed_rms(sound, pulse['t'][i], 0.2 * p1, 0.2 * p2)
+                if peak is not None and peak > 0.0:
+                    amplitude.addPoint(pulse.t[i], peak)
 
-        return him
+        return amplitude
     except Exception as e:
         raise ValueError(f"{pulse} & {sound}: not converted to AmplitudeTier.") from e
-
-
-def AmplitudeTier_create(tmin, tmax):
-    # Implement the AmplitudeTier_create logic here
-    return AmplitudeTier(tmin, tmax)
-
-class AmplitudeTier:
-    def __init__(self, tmin, tmax):
-        self.tmin = tmin
-        self.tmax = tmax
-        self.times = []
-        self.values = []
-
-    def addPoint(self, t, value):
-        self.times.append(t)
-        self.values.append(value)
 
 
 def PointProcess_Sound_getShimmer_local(pulse, thee, tmin, tmax, pmin, pmax, maximumPeriodFactor, maximumAmplitudeFactor):
