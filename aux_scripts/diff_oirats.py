545d0102d86df044f796aa6275e7cdd07bfa3635
vmonakhov <vmonakhov@ispras.ru>
2025-10-15 22:21:47 +0300

    Ideal script

--- aux_scripts/diff_oirats.py
+++ aux_scripts/diff_oirats.py
@@ -1,28 +1,65 @@
+import collections
 import re
 from string import punctuation as puncts
 import numpy as np
+from difflib import Differ
 from rapidfuzz.distance.JaroWinkler import distance as jw
 from pdb import set_trace as A
 
-'''
-cases = []
+line = '=' * 55
+dash = '-' * 5
 
-sentence = {
-    'numbs': [],
-    'words': [],
-    'links': []
-}
+list_sentence = []
+main_sentence = collections.defaultdict(list)
+list_sentence.append(main_sentence)
+diff = Differ().compare
 
-links = {
-    'numbs': [],
-    'words': [],
-    'diffs': []
-}
 
-def compile_diffs(main_numb, main_word, numbs, words, diffs):
-    if
+def compile_diffs(twin_dict, to_log=False):
 
-'''
+    def diff_words(word1, word2):
+        if word1.lower() == word2.lower():
+            return None
+
+        result = []
+        (sign_, _, char_) = '###'
+        delta = diff(word1.lower(), word2.lower())
+
+        for (sign, _, char) in delta:
+            if sign_ == '-' and sign == '+':
+                result.append(f'{char_} -> {char}')
+            (sign_, char_) = (sign, char)
+
+        return result or None
+
+    twin_sentence = {}
+    list_sentence.append(twin_sentence)
+
+    for twin1, twin2 in twin_dict.items():
+        if twin1 is not None:
+            (i1, word1) = twin1
+            # If we found twins
+            if twin2 is not None:
+                (i2, word2, twin_dist) = twin2
+                twin_diff = diff_words(word1, word2)
+                main_sentence[(i1, word1)].append((i2, word2, twin_dist, twin_diff))
+                twin_sentence[(i2, word2)] = (i1, word1, twin_dist, twin_diff)
+                if to_log:
+                    dist = '>' if twin_dist else '='
+                    diff_ = f'(+/-) {twin_diff}' if twin_diff else ''
+
+                    print(f"{i1:>2}: {word1:<12} ({dist}) {i2:>2}: {word2:<12} {diff_}")
+            else:
+                main_sentence[twin1].append(None)
+                if to_log:
+                    print(f"{i1:>2}: {word1:<12} (-)  {dash}")
+        else:
+            # A new word, or it is too far from its twin
+            for (i2, word2) in twin2:
+                twin_sentence[(i2, word2)] = None
+                if to_log:
+                    print(f" {dash:<15} (+) {i2:>2}: {word2:<12}")
+
 
 def split_words(text):
     parts = re.split(f'[{puncts}\\s]+', text)
@@ -38,53 +75,32 @@ def reversion(word1, word2):
     return same
 
 
-def neighbor(i1, i2, max_shape, delta=0):
-    skip = 4  # no more words between
-    dist = abs(i1 - i2) - 1 - delta
+def neighbor(i1, i2, max_shape):
+    skip = 4  # no more than four words between
+    dist = abs(i1 - i2)
     # Returns extra-big distance if we are not neighbours yet,
     # a real distance if we are neighbours now and
     # None if we are not neighbours already
     return (
-        dist if dist <= skip else
+        dist if dist <= skip + 1 else
         max_shape if i2 < i1 else
         None)
 
 
-def print_result(twin_psns):
-    for twin1, twin2 in twin_psns.items():
-        if twin1 is not None:
-            (i1, word1) = twin1
-            # If we found twins
-            if twin2 is not None:
-                (i2, word2) = twin2
-                print(f"{i1:>2}: {word1:<12} (>) {i2:>2}: {word2}")
-            else:
-                print(f"{i1:>2}: {word1:<12} (-)")
-        else:
-            # A new word, or it is too far from its twin
-            for (i2, word2) in twin2:
-                print(f"{'(+)':>20} {i2:>2}: {word2}")
-
-
 text_vars = list()
-text_dist = list()
+diffs_total = list()
 
 text_base = "Я помню чудное мгновенье, передо мной явилась ты"
-text_vars.append("Я   чудное   мгновенье пока ещё помню... передо мной явилась ты!")
-text_vars.append("Припоминаю я чудесное мгновение, впереди меня ты появилась.")
-text_vars.append("Чудо помню мгновенное, ты появилась предо мною, потом забыл я.")
-text_vars.append("Как гений чистой красоты, явилась ты, помню я, передо мной")
+text_vars.append("Ещё мгновение чудное, впереди меня когда-то появилась ты, я помню")
 
 word_bases = split_words(text_base)
 
-delimiter = '=' * 40
-print(delimiter)
+print(line)
 
 for n, text in enumerate(text_vars, 1):
     word_vars = split_words(text)
     mtrx_shape = (len(word_bases), len(word_vars))
     word_match = np.zeros(mtrx_shape, dtype=int)
-    # text_dist.append(word_match)
     max_shape = max(mtrx_shape)
 
     # Getting initial matrix of similarities
@@ -93,40 +109,51 @@ for n, text in enumerate(text_vars, 1):
             word_match[i1, i2] = int(reversion(word1, word2))
 
     # Positions of words which have no similarities by rows and by columns
-    orphans1 = set([i for i, row in enumerate(word_match) if not sum(row)])
-    orphans2 = set([j for j, col in enumerate(np.transpose(word_match)) if not sum(col)])
-    twin_dict = {}
+    holes1 = set([i for i, row in enumerate(word_match) if not sum(row)])
+    holes2 = set([j for j, col in enumerate(np.transpose(word_match)) if not sum(col)])
 
-    for step in "getting_orphans", "getting_twins":
+    for step in "getting_holes", "getting_twins":
 
-        # Updating orphans sets if some similarities actually are not neighbours
+        # Updating holes sets if some similarities actually are not neighbours
         if step == "getting_twins":
-            orphans1.update([i for (i, _), twin in twin_dict.items() if twin is None])
-            # Reinitializing twin_dict with found loners
-            twin_dict = {
-                None: [loner for loner in enumerate(word_vars) if loner not in twin_dict.values()]
-            }
-            orphans2.update([j for (j, _) in twin_dict[None]])
+            holes1.update([i for (i, _), twin in twin_dict.items() if twin is None])
+            holes2.update([j for (j, w) in enumerate(word_vars) if (j, w) not in
+                           [v[:-1] for v in twin_dict.values() if v is not None]])
+
+        twin_dict = {}
 
         for i1, word1 in enumerate(word_bases):
-            twin_dist = max_shape
+            twin_posn = twin_dist = max_shape
             twin_dict[(i1, word1)] = None
 
             for i2, word2 in enumerate(word_vars):
-                # Number of orphans between i1 and i2
-                delta = sum([(i1 < i < i2 or i2 < i < i1) for i in list(orphans1) + list(orphans2)])
-                cur_dist = neighbor(i1, i2, max_shape, delta)
+                # Number of holes before i2(!)
+                delta1 = sum([(i < i2) for i in list(holes2)])
+                # Number of holes before i1(!)
+                delta2 = sum([(j < i1) for j in list(holes1)])
+                cur_dist = neighbor(i1 + delta1, i2 + delta2, max_shape)
 
                 # If we are neighbours now or will be in future and
                 # current distance is less than a found one
                 if cur_dist is not None and cur_dist < twin_dist:
                     if word_match[i1, i2]:
                         twin_dist = cur_dist
-                        twin_dict[(i1, word1)] = (i2, word2)
-                elif cur_dist != max_shape:
+                        twin_posn = i2
+                        twin_dict[(i1, word1)] = (twin_posn, word2, twin_dist)
+                elif cur_dist != max_shape:  # None or a bigger distance value
                     break
 
-    print_result(twin_dict)
-    print(f'\n{sorted(orphans1)=} {sorted(orphans2)=}\n')
+            if step == "getting_twins":
+                if 0 < twin_dist < max_shape:
+                    holes1.add(i1)
+                    holes2.add(twin_posn)
+
+        if step == "getting_twins":
+            twin_dict[None] = [loner for loner in enumerate(word_vars) if loner not in
+                               [v[:-1] for v in twin_dict.values() if v is not None]]
+
+    compile_diffs(twin_dict, to_log=True)
+    diffs_total.append(twin_dict)
+    print(f'\n{sorted(holes1)=} {sorted(holes2)=}\n')
     #print(word_match)
-    print(delimiter)
+    print(line)
